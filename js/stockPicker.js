039e71fbe95802ef40352281b595ba62a6dac488
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-06 15:51:32 -0400

    js and python working

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,24 @@
 exports.picker = function(prices) {
+    let differenceTracker = []
+    let ans = []
 
+    for(i = 0; i < prices.length; i++){
+        for(j = 0; j < prices.length; j++){
+            if (i < j){
+                differenceTracker.push([parseInt(prices[i])-parseInt(prices[j]), i, j])
+            }
+            
+        }
+        
+    }
+
+    temp = differenceTracker[0][0]
+    for(item of differenceTracker){
+        if (item[0] < temp){
+            temp = item[0]
+            ans = [item[1], item[2]]
+        }
+
+    }
+    return ans
 }
